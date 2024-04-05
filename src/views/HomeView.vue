94f9ef547e5442984a1bee29ee2200e7b7e4f199
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2024-04-05 07:23:45 +0630

    Create TagCloud component with css style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,13 @@
         {{error}}
       </div>
       
-      <div v-if="posts.length>0">
-        <PostList :posts="posts"></PostList>
+      <div v-if="posts.length>0" class="layout">
+        <div>
+          <PostList :posts="posts"></PostList>
+        </div>
+        <div>
+          <TagCloud></TagCloud>
+        </div>
       </div>
 
       <div v-else>
@@ -16,6 +21,7 @@
 </template>
 
 <script>
+import TagCloud from '../components/TagCloud'
 import Spinner from '../components/Spinner'
 import { ref } from 'vue';
 
@@ -26,6 +32,7 @@ import PostList from '../components/PostList'
 import getPosts from '../composables/getPosts'
 export default {
   components: {
+    TagCloud,
     Spinner, PostList },
   setup(){
     let {posts,error,load}=getPosts();
@@ -39,6 +46,11 @@ export default {
     max-width: 1200px;
     margin: 0 auto;
     padding: 10px;
+  }
+  .layout{
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 20px;
   }
 </style>
 
--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,21 +1,30 @@
 <template>
-  <div v-if="posts.length">
-    <PostList :posts="filteredPosts"></PostList>
+  
+  <div class="tag">
+    <div v-if="posts.length" class="layout">
+    <div>
+      <PostList :posts="filteredPosts"></PostList>
+    </div>
+    <div>
+      <TagCloud></TagCloud>
+    </div>
   </div>
 
   <div v-else-if="error">{{error}}</div>  
 
   <div v-else>Loading ...</div>
-  
+  </div>
 
 </template>
 
 <script>
+import TagCloud from '../components/TagCloud'
 import PostList from '../components/PostList'
 import { computed } from 'vue';
 import getPosts from '../composables/getPosts'
 export default {
-  components: { PostList },
+  components: {
+    TagCloud, PostList },
     props:["tag"],
     setup(props){
         let {posts,error,load}=getPosts();
@@ -32,5 +41,8 @@ export default {
 </script>
 
 <style>
-
+  .tag{
+    max-width: 1200px;
+    margin: 0 auto;
+  }
 </style>
