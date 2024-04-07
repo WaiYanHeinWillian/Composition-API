26bb0ea0dfa43853f7eb178dfb1cedd5e4b9fa3c
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2024-04-07 07:52:02 +0630

    fetch single post document(Firebase)

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,5 +1,5 @@
 import { ref } from "vue";
-
+import {db} from '../firebase/config'
 let getPost=(id)=>{
     let post=ref(null);
     let error=ref('');
@@ -8,12 +8,15 @@ let getPost=(id)=>{
             // await new Promise((resolve,reject)=>{
             //     setTimeout(resolve, 2000);
             // })
-            let response= await fetch('http://localhost:3000/posts/'+id);
-            if(response.status===404){
-                throw new Error('This is something wrong!')
-            }
-            let data= await response.json();
-            post.value=data
+            // let response= await fetch('http://localhost:3000/posts/'+id);
+            // if(response.status===404){
+            //     throw new Error('This is something wrong!')
+            // }
+            // let data= await response.json();
+            // post.value=data
+            let res= await db.collection('posts').doc(id).get()
+            // console.log(res.id,res.data())
+            return post.value={id:res.id,...res.data()}
         }catch(err){
             error.value=err.message
         }
